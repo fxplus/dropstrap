d2a2fd07262101e3f933b27bb552a9efb1da1743
Tom Readings <digitisatom@gmail.com>
2014-11-28 11:51:14 +0000

    flatly bootswatch imported

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function (grunt) {
                     noLineComments: false,
                     outputStyle: 'nested'
                 }
-            }
+            },
             prod: {
                 options: {
                     sassDir: 'custom-sass',
