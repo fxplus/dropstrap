820e7495a194accb5d120a49dc7a0e4febf4ef40
Tom Readings <digitisatom@gmail.com>
2014-11-28 11:00:09 +0000

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            src: {
+                files: ['**/*.scss', '**/*.php'],
+                tasks: ['compass:dev']
+            },
+           options: {
+                livereload: true,
+            },
+        },
+        compass: {
+            dev: {
+                options: {
+                    sassDir: 'custom-sass',
+                    cssDir: 'css',
+                    imagesPath: 'images',
+                    noLineComments: false,
+                    outputStyle: 'nested'
+                }
+            }
+            prod: {
+                options: {
+                    sassDir: 'custom-sass',
+                    cssDir: 'css',
+                    imagesPath: 'images',
+                    noLineComments: true,
+                    outputStyle: 'compressed'
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+#Strapping
+
+
+Drupal theme - subtheme of Bootstrap:
+
+https://www.drupal.org/project/bootstrap
+
+Modified to use sass instead of less, with grunt and compass using recommendations by Deeson:
+
+https://www.deeson.co.uk/labs/using-grunt-bootstrap-compass-and-sass-drupal-sub-theme
+
+
